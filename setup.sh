ab2cd7762965e0d017a00477fc5a0e7d6920e328
LiquidNexus <itzliquidnexus@gmail.com>
2024-05-02 13:03:54 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -24,7 +24,7 @@ install_alacritty() {
 
     # Install Rust using rustup
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-    source /home/$USER/.cargo/env
+    . "/home/$USER/.cargo/env"
 
     # Update Rust
     rustup override set stable
